e993f0fb2342304f08d119d08f9e57b0ae072e55
Anastasiia Tamonova <ice_asa@mail.ru>
2022-06-20 22:15:26 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        double ticket = 5_234.50;
+        int toExMile = 20;
+
+        int exMile = (int)ticket/toExMile;
+
+        System.out.println((int)ticket);
+    }
+}
