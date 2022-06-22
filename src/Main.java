791914960c36755cc14c4f4458a70ce0437e0247
Anastasiia Tamonova <ice_asa@mail.ru>
2022-06-22 14:28:42 +0300

    Add Metode Calculate-BonusMiles

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double ticket = 5_234.50;
-        int toExMile = 20;
-
-        int exMile = (int) ticket / toExMile;
-
-        System.out.println((int) ticket);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
